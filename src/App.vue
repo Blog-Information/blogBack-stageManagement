3b1c179d128573fab85eed11d20074f8cefe97ff
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-05-19 20:41:24 +0800

    vue主要入口文件

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import navigation from '@/components/navigation.vue'
+import {defineComponent} from "vue";
+import Navigation from "@/components/navigation.vue";
+export default defineComponent({
+  components: {Navigation},
+  component:{
+    navigation
+  }
+})
+</script>
+<template>
+  <p>test</p>
+  <navigation>
+  </navigation>
+</template>
+<style>
+
+</style>
